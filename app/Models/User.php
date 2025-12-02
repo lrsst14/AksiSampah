c52fa4cda543f55233e9aa3ba73f5421f97356f5
alenas2 <alensaputri272@gmail.com>
2025-12-02 13:46:36 +0700

    siang

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
     protected $table = 'users';
     protected $primaryKey = 'id_users';
     public $incrementing = true;
-    protected $keyType = 'int';
+    protected $keyType = 'bigint';
 
     protected $fillable = [
         'name',
