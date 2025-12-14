58c3c8ded3160ab8cb4def236195c91d73b56a09
Larasati Dwi <larasatidw32@gmail.com>
2025-12-14 22:15:47 +0700

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,22 +8,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        $this->app->singleton(
-            \Laravel\Fortify\Contracts\RegisterResponse::class,
-            \App\Responses\Fortify\CustomRegisterResponse::class
-        );
-
-        $this->app->singleton(
-            \Laravel\Fortify\Contracts\LoginResponse::class,
-            \App\Responses\Fortify\CustomLoginResponse::class
-        );
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
